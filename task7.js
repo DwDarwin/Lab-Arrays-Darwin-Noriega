7f309ec3c47a9296dc92a9fa2a2773b8246f1cfd
darwin noriega <dwmotog5@gmail.com>
2026-09-22 16:15:45 -0500

    entrega ejercicio 7

--- task7.js
+++ task7.js
@@ -5,7 +5,18 @@ const arr = [
   ]
   
   // ¡Escribe tu código debajo de esta línea!
-  
-  
+   //* ¿Pueden agregar un solo número a una fila existente?
+    arr[0].push(10)
+
+   //  * ¿Pueden agregar una fila completa de números?
+   arr.push([30,31,32,33,34,35,36,37,38,39])
+
+  //  * ¿Pueden eliminar un solo número de una sola fila?
+   arr[1].splice(0, 1)
+
+  //  * ¿Pueden invertir una de las filas sin afectar a las demás?
+   arr[3].reverse()
+
+  console.log(arr)
   
   // ¡Escribe tu código encima de esta línea!
